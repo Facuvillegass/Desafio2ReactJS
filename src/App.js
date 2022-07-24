7baa7f7ef124d5f690d96f61e3767f47a6fcf3b8
Facundo Villegas <facundovillegas746@gmail.com>
2022-07-24 18:11:03 -0300

    subo nuevo desafío React JS

--- src/App.js
+++ src/App.js
@@ -13,13 +13,7 @@ function App() {
       </header>
       <main>
         <HomePage />
-        <ItemListContainer title= "ArmBot" image = "/assets/armBot.png" description = "Need a hand? Here's your solution!" />
-        <ItemListContainer title= "BarBot" image = "/assets/barBot.png" description = "Need a drink? Just ask him!" />
-        <ItemListContainer title= "BotEye" image = "/assets/botEye.png" description = "Can't see well? Let him do it!" />
-        <ItemListContainer title= "FriendBot" image = "/assets/friendBot.png" description = "Sick of people? Call him!" />
-        <ItemListContainer title= "PrintBot" image = "/assets/printBot.png" description = "You talk, he prints!" />
-        <ItemListContainer title= "StoryTeller" image = "/assets/storyTeller.png" description = "Tired of reading? Let him do the work!" />
-     
+        <ItemListContainer />
       </main>
     </div>
   );
--- src/ItemListContainer/ItemListContainer.js
+++ src/ItemListContainer/ItemListContainer.js
@@ -1,15 +1,33 @@
 import "./ItemListContainer.css"
-import ItemCount from "../ItemCount/ItemCount";
+import { useState, useEffect } from "react";
+import products from "../utils/products.js"
+import ItemList from "../ItemList/ItemList";
 
-function ItemListContainer ({title, image, description}) {
+function ItemListContainer () {
+    const [itemProducts, setItemProducts] = useState([])
+
+    const dataPromise = new Promise ((resolve,reject) => {
+        setTimeout(()=> {
+            resolve(products)
+        },2000)
+
+    })
+
+    useEffect(()=> {
+        dataPromise.then ((res) => {
+            setItemProducts(res)
+        })
+        .catch ((error)=> {
+            console.log("el error es : " + error)
+        })
+    }, [])
+   
+    
+
     return (
         <div className = "itemProductList">
-            <div className="particularItem">
-            <h2>{title}</h2>
-            <img className = "imgProductList" src= {image} alt=""/>
-            <ItemCount />
-            <p>{description}</p>
-            <button>Wanna know more!</button>
+            <div>
+                <ItemList dataProducts={itemProducts}/>
             </div>
         </div>
     )
